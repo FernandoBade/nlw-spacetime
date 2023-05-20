f71e3fead147ff0b69100ed9ed6c4fc6997bf43b
Fernando Bade <bade.fernando@gmail.com>
2023-05-20 19:11:40 -0300

    estrutura backend + CRUD

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,16 @@
-import { PrismaClient } from '@prisma/client';
-import fastify from 'fastify';
+import cors from "@fastify/cors";
+import fastify from "fastify";
+
+import { memoriesRoutes } from "./routes/memories";
 
 const app = fastify();
-const prisma = new PrismaClient();
-const localHost: string = 'http://localhost/3333';
+const localHost: string = "http://localhost/3333";
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany();
-  return users;
+app.register(cors, {
+  origin: true,
 });
+
+app.register(memoriesRoutes);
 
 app
   .listen({
